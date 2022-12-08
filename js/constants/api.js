c403a0c2c6216ad27ec9f9f3a96827e2db483b3f
Prebre <idabrekk@gmail.com>
2022-12-08 23:57:56 +0100

    Added responsiveness and interactiveness
    Added responsiveness to more elements
    
    Added interaciveness to more pages and elements
    
    Added potential visuals/icons
    
    Made some aestethic changes
    
    Added JS to js/constants/api.js
    
    Added JS to js/post-list.js
    
    Generated blog posts to post-list.html through JS
    
    Improved form

--- js/constants/api.js
+++ js/constants/api.js
@@ -1,3 +1,15 @@
+// API
+
+export const api = "https://ikpb-mar21pt-cms-ma1.com/wp-json/wp/v2/posts";
+
+
 // Key & Secret
 
-// API
+export const key = "ck_06d5d8a678531b91ce7fc969d00bcb41e9ebe144"
+
+export const secret = "cs_df85f85242b5887c35d06c1997eb5b3557efd8ea"
+
+
+// URL
+
+export const url = `${api}?consumer_key=${key}&consumer_secret=${secret}`;
--- js/post-list.js
+++ js/post-list.js
@@ -1,1 +1,32 @@
 // Post list
+
+import { api } from "./constants/api.js";
+
+const listContainer = document.querySelector("#list-container");
+
+listContainer.innerHTML = "";
+
+async function getList() {
+        try {
+                const response = await fetch(api);
+
+                const post = await response.json();
+
+                for(let i = 0; i < post.length; i++) {
+
+                        listContainer.innerHTML += `<div class="card">
+                                <a href="#"><h2>${post[i].title.rendered}</h2></a>
+                                <p>Posted by <a href="#">${post[i].author}</a> on ${post[i].date}</p>
+                                <p>${post[i].excerpt.rendered}</p>
+                                </div>`
+
+                                                        
+                }
+        }
+        catch(error) {
+                console.log("An error has occurred");
+                listContainer.innerHTML = "An error has occurred"
+        }
+}
+
+getList();
