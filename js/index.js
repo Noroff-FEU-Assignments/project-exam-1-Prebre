5cd77b1b3fb45194e604f2806efe9e212921182a
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2022-12-13 23:42:48 +0100

    Fixed topbar + js
    Fixed the topbar and nav section to be more responsive.
    
    Tried to apply a carousel of latest posts to the index page. JS is currently printing "An error has occurred" to the page, which means the error catching is working.
    
    Need to figure out what the error is and fix it.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Latest
+
+import { api } from "./constants/api.js";
+
+const latestContainer = document.querySelector("#latest-container");
+
+latestContainer.innerHTML = "";
+
+async function getLatest() {
+        try {
+                const response = await fetch(api);
+
+                const post = await response.json();
+
+                for(let i = 0; i < post.length; i++) {
+
+                        if (i === 4) {
+                            break;
+                        }
+
+                        listContainer.innerHTML += `<li class="carousel">
+                        <h3><a href="#" alt="#">${post[i].title.rendered}</a></h3><br>
+                        <img src="${post[i].jetpack_featured_media_url}" alt="#"><br>
+                        <p class="author">${post[i].author}</p> &nbsp; <p class="date">${post[i].date}</p><br>
+                        <p class="excerpt">${post[i].excerpt.rendered}</p><br>
+                        <input type="button" class="cta" onclick="location.href='#';" value="Read more" />
+                        </li>`
+
+                                                        
+                }
+        }
+        catch(error) {
+                console.log("An error has occurred");
+                latestContainer.innerHTML = "An error has occurred"
+        }
+}
+
+getLatest();
