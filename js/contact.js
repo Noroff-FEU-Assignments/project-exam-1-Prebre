48d021ceeb0f8bf54a833c8610a03b060e9da2bb
Prebre <idabrekk@gmail.com>
2023-01-19 19:10:37 +0100

    Added improved validation to contact form.
    Finally found a method that made proper validations of the contact form. Still working on some small bugs.

--- js/contact.js
+++ js/contact.js
@@ -1,50 +1,122 @@
-function validate(){
-        const name = document.querySelector("#name").value;
+const form = document.querySelector("#contactForm");
+const fullname = document.getElementById("fullname");
+const mail = document.getElementById("mail");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
 
-        const email = document.querySelector("#mail").value;
+const errorName = fullname.nextElementSibling;
+const errorMail = mail.nextElementSibling;
+const errorSubj = subject.nextElementSibling;
+const errorMsg = message.nextElementSibling;
 
-        const subject = document.querySelector("#subject").value;
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-        const message = document.querySelector("#message").value;
+window.addEventListener("load", () => {
+    const nameValid = fullname.value.length >= 5;
+    fullname.className = nameValid ? "valid" : "invalid";
 
-        const validationMsg = document.querySelector("#validation-message");
+    const mailValid = mail.value.length >= 5 && emailRegExp.test(mail.value);
+    mail.className = mailValid ? "valid" : "invalid";
 
-        validationMsg.style.padding = "10px";
+    const subjValid = subject.value.length >= 15;
+    subject.className = subjValid ? "valid" : "invalid";
 
-        validationMsg.innerHTML="";
-    
-    let validMsg;
-    if(name.length <= 5) {
-        validMsg = "Name must be at least 5 characters long";
+    const msgValid = message.value.length >= 25;
+    message.className = msgValid ? "valid" : "invalid";
 
-        validationMsg.innerHTML = validMsg;
-        return false;
+});
+
+fullname.addEventListener("input", () => {
+    const nameValid = fullname.value.length >= 5;
+    if (nameValid) {
+        fullname.className = "valid";
+        errorName.textContent = "";
+        errorName.className = "error";
     }
-
-    if(email.indexOf("@") == -1) {
-        validMsg = "Please enter a valid email address";
-
-        validationMsg.innerHTML = validMsg;
-        return false;
-    }
-
-    if(subject.length <= 15) {
-        validMsg = "Subject must be at least 15 characters long";
-
-        validationMsg.innerHTML = validMsg;
-        return false;
-    }
-    
-    if(message.length <= 25) {
-        validMsg = "Message must be at least 25 characters long";
-        
-        validationMsg.innerHTML = validMsg;
-        return false;
-    }
-
     else {
-        validMsg = "Thank you for contacting us!"
-
-        return true;
+        fullname.className = "invalid";
     }
-  }
+});
+
+mail.addEventListener("input", () => {
+    const mailValid = mail.value.length >= 5 && emailRegExp.test(mail.value);
+    if (mailValid) {
+        mail.className = "valid";
+        errorMail.textContent = "";
+        errorMail.className = "error";
+    }
+    else {
+        mail.className = "invalid";
+    }
+});
+
+subject.addEventListener("input", () => {
+    const subjValid = subject.value.length >= 15;
+    if (subjValid) {
+        subject.className = "valid";
+        errorSubj.textContent = "";
+        errorSubj.className = "error";
+    }
+    else {
+        subject.className = "invalid";
+    }
+});
+
+message.addEventListener("input", () => {
+    const msgValid = message.value.length >= 25;
+    if (msgValid) {
+        message.className = "valid";
+        errorMsg.textContent = "";
+        errorMsg.className = "error";
+    }
+    else {
+        message.className = "invalid";
+    }
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nameValid = fullname.value.length >= 5;
+    if (!nameValid) {
+        fullname.className = "invalid";
+        errorName.textContent = "Name must contain more than 5 characters";
+        errorName.className = "error active";
+    }
+    
+    const mailValid = mail.value.length >= 5 && emailRegExp.test(mail.value);
+    if (!mailValid) {
+        mail.className = "invalid";
+        errorMail.textContent = "Please enter a valid email address";
+        errorMail.className = "error active";
+    }
+
+    const subjValid = subject.value.length >= 15;
+    if (!subjValid) {
+        subject.className = "invalid";
+        errorSubj.textContent = "Subject must contain more than 15 characters";
+        errorSubj.className = "error active";
+    }
+
+    const msgValid = message.value.length >= 25;
+    if (!msgValid) {
+        message.className = "invalid";
+        errorMsg.textContent = "Message must contain more than 25 characters";
+        errorMsg.className = "error active";
+    }
+    else {
+        fullname.className = "valid";
+        mail.className = "valid";
+        subject.className = "valid";
+        message.className = "valid";
+        errorName.textContent = "";
+        errorName.className = "error";
+        errorMail.textContent = "";
+        errorMail.className = "error";
+        errorSubj.textContent = "";
+        errorSubj.className = "error";
+        errorMsg.textContent = "";
+        errorMsg.className = "error";
+    }
+
+});
