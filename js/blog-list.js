0e19215931b26cc136e59389995d7c7db7ba6ed6
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2023-02-07 20:50:35 +0100

    Added functionality to "Load More" button
    Added functionality to the "Load More" button on the blog list page. I don't think I've found the best solution, but at least it works for now, which is enough for me!

--- js/blog-list.js
+++ js/blog-list.js
@@ -6,6 +6,8 @@ const postsPerPage = `${posts}?per_page=100&page=1`;
 
 const listContainer = document.querySelector("#list-container");
 
+const loadBtn = document.querySelector("#loadPosts");
+
 listContainer.innerHTML = "";
 
 async function getList() {
@@ -16,13 +18,27 @@ async function getList() {
 
     for (let i = 0; i < 10; i++) {
       listContainer.innerHTML += `<div class="card">
-                                <a href="post.html?id=${post[i].id}"><h2>${post[i].title.rendered}</h2></a>
-                                <div class="list-img-container">
-                                        <img src="${post[i].jetpack_featured_media_url}" class="list-img" alt="Image to visualise the theme of this post, ${post[i].title.rendered}">
-                                </div>
+                            <a href="post.html?id=${post[i].id}"><h2>${post[i].title.rendered}</h2></a>
+                            <div class="list-img-container">
+                                <img src="${post[i].jetpack_featured_media_url}" class="list-img" alt="Image to visualise the theme of this post, ${post[i].title.rendered}">
+                            </div>
                                 ${post[i].excerpt.rendered}
-                                </div>`;
+                            </div>`;
     }
+
+    loadBtn.addEventListener("click", function loadTen() {
+      for (let j = 10; j < 20; j++) {
+        listContainer.innerHTML += `<div class="card">
+                            <a href="post.html?id=${post[j].id}"><h2>${post[j].title.rendered}</h2></a>
+                            <div class="list-img-container">
+                                <img src="${post[j].jetpack_featured_media_url}" class="list-img" alt="Image to visualise the theme of this post, ${post[j].title.rendered}">
+                            </div>
+                                ${post[j].excerpt.rendered}
+                            </div>`;
+      }
+
+      loadTen();
+    });
   } catch (error) {
     console.log("An error has occurred");
     listContainer.innerHTML = "An error has occurred";
