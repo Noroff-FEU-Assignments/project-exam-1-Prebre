947330df19938144f715695a0ec2a1c14492e587
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2023-02-07 22:41:21 +0100

    Tried adding category links to blog-list.html
    Tried to add category links to blog-list.html that allows the user to view blogs from categories. It didn't work at first, and I realized I was fetching data from the wrong endpoint. Need to figure out how to fetch from the correct endpoint.

---
+++ js/category-links.js
@@ -0,0 +1,24 @@
+// Category links
+
+import { categories } from "./constants/api.js";
+
+const linkContainer = document.querySelector("#link-container");
+
+linkContainer.innerHTML = "";
+
+async function getLinks() {
+  try {
+    const response = await fetch(categories);
+
+    const category = await response.json();
+
+    for (let i = 0; i < 10; i++) {
+      linkContainer.innerHTML += `<a class="cta" href="/category.html?${category[i].id}">${category[i].name}</a>`;
+    }
+  } catch (error) {
+    console.log("An error has occurred");
+    linkContainer.innerHTML = "An error has occurred";
+  }
+}
+
+getLinks();
